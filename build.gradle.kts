b37d05326fe87558ead380fcb160a0200776868a
root <root@t>
2019-11-17 15:36:14 +0800

    first commit

---
+++ build.gradle.kts
@@ -0,0 +1,56 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    java
+    application
+    id("org.jetbrains.kotlin.jvm") version "1.3.60"
+    id("com.github.johnrengelman.shadow") version "5.2.0"
+}
+
+version = "0.0.1-SNAPSHOT"
+java.sourceCompatibility = JavaVersion.VERSION_12
+
+repositories {
+	mavenCentral()
+	maven { url = uri("https://oss.sonatype.org/content/repositories/snapshots") }
+}
+
+dependencies {
+    implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+
+    testImplementation("org.jetbrains.kotlin:kotlin-test")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit")
+
+    implementation("io.vertx:vertx-core:+")
+    implementation("io.vertx:vertx-lang-kotlin:+")
+    implementation("io.vertx:vertx-web:+")
+}
+
+
+application.mainClassName="io.vertx.core.Launcher"
+val mainVerticleName="vcell.Vcell"
+
+tasks.shadowJar {
+	classifier = "fat"
+	manifest {
+		attributes["Main-Verticle"] = mainVerticleName
+	}
+	mergeServiceFiles {
+		include ("META-INF/services/io.vertx.core.spi.VerticleFactory")
+	}
+}
+
+tasks.withType<KotlinCompile> {
+	kotlinOptions {
+		freeCompilerArgs = listOf("-Xjsr305=strict")
+		jvmTarget = "12"
+	}
+}
+
+// gradle wrapper --gradle-version 5.6.3 --distribution-type all
+
+tasks.wrapper {
+    gradleVersion = "6.0"
+    distributionType = Wrapper.DistributionType.ALL
+}
