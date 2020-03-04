ded0baf5d6c0376fedf53f1ff8e391b3300a951a
root <root@t>
2020-03-05 02:05:30 +0800

    up

--- build.gradle.kts
+++ build.gradle.kts
@@ -58,6 +58,6 @@ tasks.withType<KotlinCompile> {
 // gradle wrapper --gradle-version 5.6.3 --distribution-type all
 
 tasks.wrapper {
-    gradleVersion = "6.2.1"
+    gradleVersion = "6.2.2"
     distributionType = Wrapper.DistributionType.ALL
 }
