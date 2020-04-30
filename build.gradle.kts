d38a4d4dd91e1ed123ed97a60157e06e4ef73ed5
root <root@t>
2020-04-30 09:58:45 +0800

    up

--- build.gradle.kts
+++ build.gradle.kts
@@ -34,7 +34,7 @@ repositories {
 
 dependencies {
     implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib")
 
     testImplementation("org.jetbrains.kotlin:kotlin-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit")
